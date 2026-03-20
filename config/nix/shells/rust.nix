4101cc4d8a83bebd54a6c780102f36913838b020
ca971 <discretgwad@gmail.com>
2026-03-20 10:11:32 +0100

    fix(nix): simplify rust shell, remove apple SDK deps

--- config/nix/shells/rust.nix
+++ config/nix/shells/rust.nix
@@ -4,10 +4,9 @@ pkgs.mkShell {
   name = "rust-dev";
 
   buildInputs = with pkgs; [
-    # Rust toolchain (latest stable via overlay)
+    # Rust toolchain
     (rust-bin.stable.latest.default.override {
       extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
-      targets = [ "wasm32-unknown-unknown" ];
     })
 
     # Build dependencies
@@ -18,21 +17,12 @@ pkgs.mkShell {
     # Tools
     cargo-watch
     cargo-edit
-    cargo-expand
-    cargo-audit
     cargo-nextest
     sccache
-
-    # WASM
-    wasm-pack
-  ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.apple-sdk.frameworks; [
-    Security
-    SystemConfiguration
-  ]);
+  ];
 
   env = {
     RUST_BACKTRACE = "1";
-    RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
   };
 
   shellHook = ''
